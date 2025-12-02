89fba29ae2b05c32e17f2398924941e0fc161dfe
Siddhant Gupta <guptasiddhant@icloud.com>
2025-12-02 20:55:39 +0200

    feat(vercel): add UI

--- apps/vercel-app/app.ts
+++ apps/vercel-app/app.ts
@@ -1,11 +1,13 @@
-import "hono";
+import "hono"; // import hono to trick vercel into recognizing hono apps
 
 import { createHonoRouter } from "@storybooker/core";
 import { LocalFileDatabase, LocalFileStorage } from "@storybooker/core/adapter/fs";
+import { createBasicUIAdapter } from "@storybooker/ui";
 
 const app = createHonoRouter({
   database: new LocalFileDatabase(),
   storage: new LocalFileStorage(),
+  ui: createBasicUIAdapter({ logo: "/vercel.png" }),
 });
 
 export default app;
