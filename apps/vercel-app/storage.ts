435ff34814377b0bb1ccbfa0ca44e11b5b319f51
Siddhant Gupta <guptasiddhant@icloud.com>
2025-12-02 20:46:05 +0200

    feat: add vercel config

--- apps/vercel-app/storage.ts
+++
@@ -1,4 +0,0 @@
-import { StorageAdapterErrors, type StorageAdapter } from "@storybooker/core/adapter";
-import * as vb from "@vercel/blob";
-
-export class VercelBlobStorage implements StorageAdapter {}
